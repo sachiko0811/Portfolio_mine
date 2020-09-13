b9f96ec7eff5087da566c9483c4cba533192227c
ShosukeDoi <doiverson.canada@gmail.com>
2020-09-13 16:49:55 -0700

    added webpack

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const FixStyleOnlyEntriesPlugin = require('webpack-fix-style-only-entries');
+const HtmlWebpackPlugin =  require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    entry: {
+        bundle: './js/index.js',
+        style: './stylesheets/scss/style.scss',
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name].js',
+        chunkFilename: 'js/[name].[id].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    MiniCssExtractPlugin.loader, 'css-loader'
+                ]
+            },
+            {
+                test: /\.scss$/,
+                use: [
+                    {loader: MiniCssExtractPlugin.loader},
+                    {loader: 'css-loader'},
+                    {loader: 'sass-loader'},
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({filename: 'css/[name].css'}),
+        new FixStyleOnlyEntriesPlugin(),
+        // new CopyWebpackPlugin([
+        //     {
+        //         from: './public',
+        //         to: './'
+        //     },
+        // ]),
+        new HtmlWebpackPlugin({
+            template: './index.html'
+        }),
+    ],
+    optimization: {
+        minimizer: [new OptimizeCSSAssetsPlugin({})]
+    },
+    devServer: {
+        contentBase: './',
+        watchContentBase: true,
+        openPage: "index.html",
+        compress: true,
+        open: true,
+        port: 3000,
+        inline: true
+    }
+};
