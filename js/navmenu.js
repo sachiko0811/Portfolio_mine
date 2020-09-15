9e9ef57b55ea5124f9ab80d66b3b605bab87be69
sachiko1995 <sachikonovel@yahoo.co.jp>
2020-09-14 18:47:33 -0700

    nav

--- js/navmenu.js
+++ js/navmenu.js
@@ -2,4 +2,6 @@ $(function() {
     $('.nav-toggle').on('click', function() {
         $('.nav-toggle, .nav-second-nav').toggleClass('show');
     });
+
+    $('#container').load('../html/nav.html');
 })
