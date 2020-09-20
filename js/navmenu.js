f56f83ee9e3fd57308a22d5be97f763a2ad85c29
sachiko1995 <sachikonovel@yahoo.co.jp>
2020-09-20 13:26:18 -0700

    added scss and js

--- js/navmenu.js
+++ js/navmenu.js
@@ -2,10 +2,18 @@ $(function() {
 
     $('#container').load('../html/nav.html');
 
-    $('.nav-toggle').on('click', function() {
-        console.log('VicRoy: ')
-        $('.nav-toggle, .nav-second-nav').toggleClass('show');
-    });
+    // $('.nav-toggle').on('click', function() {
+    //     console.log('VicRoy: ')
+    //     $('.nav-toggle, .nav-second-nav').toggleClass('show');
+    // });
+
+    $(function(){
+        $('.btn-trigger').on('click', function() {
+          $(this).toggleClass('active');
+          $('.nav-toggle, .nav-second-nav').toggleClass('show');
+          return false;
+        });
+      });
 
 })
 
--- js/navmenutoggle.js
+++ js/navmenutoggle.js
@@ -1,9 +1,17 @@
 $(function() {
 
-    $('.nav-toggle').on('click', function() {
-        console.log('VicRoy: ')
-        $('.nav-toggle, .nav-second-nav').toggleClass('show');
-    });
+    // $('.nav-toggle').on('click', function() {
+    //     console.log('VicRoy: ')
+    //     $('.nav-toggle, .nav-second-nav').toggleClass('show');
+    // });
+
+    $(function(){
+        $('.btn-trigger').on('click', function() {
+          $(this).toggleClass('active');
+          $('.nav-toggle, .nav-second-nav').toggleClass('show');
+          return false;
+        });
+      });
 
 })
 
