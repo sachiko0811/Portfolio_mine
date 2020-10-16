4b2aa4dfcfafe25624378f59bcfa7cf6cdd87899
sachiko1995 <sachikonovel@yahoo.co.jp>
2020-10-16 11:39:01 -0700

    about html typed

---
+++ js/typed.js
@@ -0,0 +1,9 @@
+ityped.init(document.querySelector("#ityped"), {
+    strings: [ 'I love coding!', 'Stay happy, stay positive.', 'I love taking photos!'],
+    startDelay: 150,                                                
+    typeSpeed: 200,                                                
+    backSpeed:  95,                                                
+    backDelay:  150,                                                
+    showCursor: true,                               
+    cursorChar: "|",      
+})
