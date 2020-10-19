d58533452e8bf6d45323da74b22e3f2199b8bec9
ShosukeDoi <doiverson.canada@gmail.com>
2020-10-18 17:27:22 -0700

    remove fadeInDown class before jumping to another section

--- js/navmenutoggle.js
+++ js/navmenutoggle.js
@@ -8,6 +8,16 @@ $(function() {
     $(function(){
         $('.btn-trigger').on('click', function() {
           $(this).toggleClass('active');
+
+          // remove "fadeInDown" class before jumping to another section
+          if ($(this).hasClass('active')) {
+              let nodeList = document.querySelectorAll('.fadeInDown');
+              let items = Array.from(nodeList);
+              items.map(i => {
+                  i.classList.remove('fadeInDown')
+              })
+          }
+
           $('.nav-toggle, .nav-second-nav').toggleClass('show');
           document.body.classList.toggle('popup-stop-scroll')
           return false;
